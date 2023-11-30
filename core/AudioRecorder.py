f97299a85525b337864bf32028fffa9130aef9f3
Joshpren <102427710+Joshpren@users.noreply.github.com>
2023-11-30 22:11:06 +0100

    Update AudioRecorder.py

--- core/AudioRecorder.py
+++ core/AudioRecorder.py
@@ -7,54 +7,45 @@ from datetime import datetime
 
 class AudioRecorder:
 
-    form_1 = pyaudio.paInt16 # 16-bit resolution
-    chans = 1 # 1 channel
-    samp_rate = 44100 # 44.1kHz sampling rate
-    chunk = 4096 # 2^12 samples for buffer
-    max_audio_length = 300 # max seconds to record
+    form_1 = pyaudio.paInt16
+    chans = 1
+    samp_rate = 44100
+    chunk = 4096
+    max_audio_length = 300
 
     def __init__(self, dev_index, is_picked_up, storage_directory='resources/target'):
         self.logger = logging.getLogger('AudioRecorder')
         self.__storage_directory = storage_directory
-        self.__audio = pyaudio.PyAudio()  # create pyaudio instantiation
-        self.__dev_index = dev_index# device index found by p.get_device_info_by_index(ii)
+        self.__audio = pyaudio.PyAudio()
+        self.__dev_index = dev_index
         self.__frames = []
         self.__duration = 0
         self.__is_picked_up = is_picked_up
 
     def record(self):
-        """ Record audio """
         if not self.__is_picked_up:
             return
         print("recording")
         try:
-            # Open audio stream
-            stream = self.__audio.open(format=self.form_1, rate=self.samp_rate, channels=self.chans, \
-                                              input_device_index=self.__dev_index, input=True, \
-                                              frames_per_buffer=self.chunk)
-            # Start timer
+            stream = self.__audio.open(
+                format=self.form_1, rate=self.samp_rate, channels=self.chans,
+                input_device_index=self.__dev_index, input=True,
+                frames_per_buffer=self.chunk
+            )
             start_time = time.time()
             while self.__is_picked_up.is_set():
                 if int(time.time() - start_time) >= self.max_audio_length:
-                    # if duration reaches max audio length, then stop recording
                     print(f"Recording has been stopped after exceeding the maximum length of {self.max_audio_length} seconds!")
                     self.logger.info(f"Recording has been stopped after exceeding the maximum length of {self.max_audio_length} seconds!")
                     self.__duration = time.time() - start_time
                     break
-
                 data = stream.read(self.chunk)
                 self.__frames.append(data)
-
             self.__duration = time.time() - start_time
         except Exception as e:
-            self.logger.exception(e)
-            stream.stop_stream()
-            self.logger.debug("Stream stopped due to exception")
-            stream.close()
-            self.logger.debug("Stream closed due to exception")
+            self.logger.exception(f'Got exception on main handler: {e}')
         finally:
-            # Recording has been stopped -> stop stream
-            if stream:
+            if 'stream' in locals():
                 stream.stop_stream()
                 self.logger.debug("Stream stopped")
                 stream.close()
@@ -62,11 +53,9 @@ class AudioRecorder:
         return self
 
     def save(self):
-        """ Save the file """
         if not self.__frames or self.__duration < 5:
-            # Recording is too short and wont be saved
-            print("Recording is to short to be saved! It hast to be at least 5 seconds")
-            self.logger.debug("Recording is to short to be saved! It hast to be at least 5 seconds")
+            print("Recording is too short to be saved! It has to be at least 5 seconds")
+            self.logger.debug("Recording is too short to be saved! It has to be at least 5 seconds")
             return self
 
         print("Save recording")
@@ -75,9 +64,8 @@ class AudioRecorder:
             if not os.path.exists(self.__storage_directory):
                 os.makedirs(self.__storage_directory)
                 self.logger.debug(f"No directory has been found for {self.__storage_directory} and therefore has been created!")
-            wav_output_filename = f'{self.__storage_directory}/{datetime.now()}.wav' # name of .wav file
-            # save the audio frames as .wav file
-            wavefile = wave.open(wav_output_filename,'wb')
+            wav_output_filename = f'{self.__storage_directory}/{datetime.now()}.wav'
+            wavefile = wave.open(wav_output_filename, 'wb')
             wavefile.setnchannels(self.chans)
             wavefile.setsampwidth(self.__audio.get_sample_size(self.form_1))
             wavefile.setframerate(self.samp_rate)
@@ -85,12 +73,10 @@ class AudioRecorder:
         except Exception as e:
             self.logger.critical(f'Got exception on main handler: {e}')
         finally:
-            # Close Wave-File
             wavefile.close()
             self.logger.debug("Wavefile has been closed")
         return self
 
     def close(self):
-        """ Graceful shutdown """
         self.__frames = []
         self.logger.debug(f"Reset Frames {self.__frames}")
