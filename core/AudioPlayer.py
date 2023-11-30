2ecb7aa3e8f99e1a9e137a15afa871f5cf4edf5b
Joshpren <102427710+Joshpren@users.noreply.github.com>
2023-11-30 22:07:42 +0100

    Update AudioPlayer.py

--- core/AudioPlayer.py
+++ core/AudioPlayer.py
@@ -7,19 +7,16 @@ class AudioPlayer:
     chunk = 1024
 
     def __init__(self, dev_index, is_picked_up):
-        """ Init audio stream """
-        self.logger = logging.getLogger('AudioRecoder')
+        self.logger = logging.getLogger('AudioPlayer')
         self.__dev_index = dev_index
         self.__is_picked_up = is_picked_up
         self.__audio = pyaudio.PyAudio()
 
     def play(self, file):
-        """ Play entire file """
         print("Play announcement")
         self.logger.debug(f"Play announcement \"{file}\"")
         try:
             wf = wave.open(file, 'rb')
-            data = wf.readframes(self.chunk)
             stream = self.__audio.open(
                 format=self.__audio.get_format_from_width(wf.getsampwidth()),
                 channels=wf.getnchannels(),
@@ -27,17 +24,17 @@ class AudioPlayer:
                 rate=wf.getframerate(),
                 output=True
             )
+            data = wf.readframes(self.chunk)
             while data != b'' and self.__is_picked_up.is_set():
                 stream.write(data)
                 data = wf.readframes(self.chunk)
-        except:
-            self.logger.critical('Got exception on main handler')
+        except Exception as e:
+            self.logger.critical(f'Got exception on main handler: {e}')
         finally:
-            if stream:
+            if 'stream' in locals():
                 stream.stop_stream()
                 self.logger.debug("Stream has been stopped!")
                 stream.close()
                 self.logger.debug("Stream has been closed!")
             wf.close()
             self.logger.debug("Wave-File has been closed!")
-
