a9d2ea386c36795af86f7371c32e76e0daa816fe
Joshpren <joshua.sprengard@gmail.com>
2025-04-28 18:59:45 +0200

    last quick attempt

--- src/fsm/WeddingBookMachine.py
+++ src/fsm/WeddingBookMachine.py
@@ -43,14 +43,14 @@ class WeddingBookMachine(StateMachine):
 
 
     def on_pick_up(self):
-        if not self.current_state == WeddingBookMachine.idling:
+        if self.current_state == WeddingBookMachine.recording:
             return
         logger.debug("Pick up!")
         self.is_picked_up.set()
         self.record()
 
     def on_hang_up(self):
-        if not self.current_state == WeddingBookMachine.recording:
+        if self.current_state == WeddingBookMachine.idling:
             return
         logger.debug("Hang up")
         self.is_picked_up.clear()
