4a59a970c4fb5b6edbf29499a80b678a08ffe5e7
Joshpren <joshua.sprengard@gmail.com>
2025-05-16 14:24:56 +0200

    Further testing

--- src/WeddingBook.py
+++ src/WeddingBook.py
@@ -9,11 +9,11 @@ logger = logging.getLogger(__name__)
 class WeddingBook:
 
     gpio_device = None
-    __wbm_thread = None
+    wbm_thread = None
 
     def __init__(self, config):
         self.pin_number = int(config["gpio_index"])
-        self.__wbm = WeddingBookMachine.WeddingBookMachine(config)
+        self.wbm = WeddingBookMachine.WeddingBookMachine(config)
         logger.debug(f"Starting with pin-number: {self.pin_number}")
         
         
@@ -23,12 +23,12 @@ class WeddingBook:
             time.sleep(0.5)
 
     def pick_up(self):
-        self.__wbm_thread = threading.Thread(target=self.__wbm.on_pick_up, args=())
-        self.__wbm_thread.start()
+        self.wbm_thread = threading.Thread(target=self.wbm.on_pick_up, args=())
+        self.wbm_thread.start()
 
     def hang_up(self):
-        self.__wbm.on_hang_up()
-        self.__wbm_thread.join()
+        self.wbm.on_hang_up()
+        self.wbm_thread.join()
         
 
     def gpio_setup(self, gpio_device=None):
--- tests/WeddingBookTest.py
+++ tests/WeddingBookTest.py
@@ -5,9 +5,11 @@ from gpiozero.pins.mock import MockFactory
 from time import sleep
 
 
-class MyTestCase(unittest.TestCase):
+class WeddingBookMaschineTest(unittest.TestCase):
 
-    def test_recording(self):
+
+    def test_playing(self):
+        print("Test Playing")
         Device.pin_factory  = MockFactory()
         gpio_pin = Device.pin_factory.pin(17)
         config = { "gpio_index":17, "device_index": 1 }
@@ -18,16 +20,69 @@ class MyTestCase(unittest.TestCase):
         
         # Initial state
         self.assertEqual(False, gpio_device.is_active)
-        sleep(2)
+        self.assertEqual(wedding_book.wbm.idling, wedding_book.wbm.current_state)
+        sleep(1)
         # Pick up
         print("Pick up")
         gpio_pin.drive_low()
         self.assertEqual(True, gpio_device.is_active)
+        self.assertEqual(wedding_book.wbm.playing, wedding_book.wbm.current_state)
+        sleep(6)
+        gpio_pin.drive_high()
+
+    def test_aborting_the_playing_state(self):
+        print("Test aborting the Playing-State")
+        Device.pin_factory  = MockFactory()
+        gpio_pin = Device.pin_factory.pin(17)
+        config = { "gpio_index":17, "device_index": 1 }
+        wedding_book = WeddingBook(config)
+        wedding_book.gpio_setup()
+
+        gpio_device = wedding_book.gpio_device
+        
+        # Initial state
+        self.assertEqual(False, gpio_device.is_active)
+        self.assertEqual(wedding_book.wbm.idling, wedding_book.wbm.current_state)
+        sleep(1)
+        # Pick up
+        print("Pick up")
+        gpio_pin.drive_low()
+        self.assertEqual(True, gpio_device.is_active)
+        self.assertEqual(wedding_book.wbm.playing, wedding_book.wbm.current_state)
         sleep(2)
         # Hang up
         print("Hang up")
         gpio_pin.drive_high()
         self.assertEqual(False, gpio_device.is_active)
+        self.assertEqual(wedding_book.wbm.idling, wedding_book.wbm.current_state)
+
+
+    def test_recording_and_hangup_after_4_seconds(self):
+        print("Test REcording and hang up after 4 seconds")
+        Device.pin_factory  = MockFactory()
+        gpio_pin = Device.pin_factory.pin(17)
+        config = { "gpio_index":17, "device_index": 1 }
+        wedding_book = WeddingBook(config)
+        wedding_book.gpio_setup()
+
+        gpio_device = wedding_book.gpio_device
+        
+        # Initial state
+        self.assertEqual(False, gpio_device.is_active)
+        self.assertEqual(wedding_book.wbm.idling, wedding_book.wbm.current_state)
+        sleep(1)
+        # Pick up
+        print("Pick up")
+        gpio_pin.drive_low()
+        self.assertEqual(True, gpio_device.is_active)
+        self.assertEqual(wedding_book.wbm.playing, wedding_book.wbm.current_state)
+        sleep(10)
+        self.assertEqual(wedding_book.wbm.recording, wedding_book.wbm.current_state)
+        # Hang up
+        print("Hang up")
+        gpio_pin.drive_high()
+        self.assertEqual(False, gpio_device.is_active)
+        self.assertEqual(wedding_book.wbm.idling, wedding_book.wbm.current_state)
         
 
 if __name__ == '__main__':
