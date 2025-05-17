442695960ab2dbf28a9d60c6fd4d49eef3197036
Joshpren <102427710+Joshpren@users.noreply.github.com>
2025-05-17 20:15:09 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+sudo apt-get install portaudio19-dev python3-pyaudio
+python -m venv /etc/wedding-book/.venv --system-site-packages
+source /etc/wedding-book/.venv/bin/activate
+pip install -r /etc/wedding-book/requirements.txt
+deactivate
+
+
 # Erstelle die wedding-book.service Datei
 sudo bash -c 'cat > /lib/systemd/system/wedding-book.service <<EOF
 [Unit]
@@ -9,7 +16,7 @@ After=multi-user.target
 [Service]
 Type=simple
 WorkingDirectory=/etc/wedding-book/
-ExecStart=python /etc/wedding-book/main.py
+ExecStart=/etc/wedding-book/.venv/bin/python /etc/wedding-book/main.py
 Environment=PYTHONPATH=/etc/wedding-book/src/
 KillSignal=SIGINT
 Restart=on-failure
